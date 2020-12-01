dd6876c0303b52404dbd16fab5da5c5e79451e96
Mijeong Ban <mban1@stevens.edu>
2020-12-01 11:36:51 -0500

    added categorized y values on preprocessing

---
+++ src/cnn.py
@@ -0,0 +1,1 @@
+X_train, y_train, X_val, y_val, X_test, y_test, y_test_categorized = preprocessing()
--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 
 
 def preprocessing():
@@ -31,14 +32,20 @@ def preprocessing():
     X_val = pad_sequences(X_val, padding="post", maxlen=maxlen)
     X_test = pad_sequences(X_test, padding="post", maxlen=maxlen)
 
+    # one-hot encode for category labels --> can be removed later nate
+    y_train = to_categorical(y_train)
+    y_val = to_categorical(y_val)
+    y_test_categorized = to_categorical(y_test)
+
     print("Shape of X_train", str(X_train.shape))
     print("Shape of y_train", str(y_train.shape))
     print("Shape of X_val", str(X_val.shape))
     print("Shape of y_val", str(y_val.shape))
     print("Shape of X_test", str(X_test.shape))
     print("Shape of y_test", str(y_test.shape))
+    print("Shape of y_test_categorized", str(y_test_categorized.shape))
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    return X_train, y_train, X_val, y_val, X_test, y_test, y_test_categorized
 
 
 if __name__ == "__main__":
