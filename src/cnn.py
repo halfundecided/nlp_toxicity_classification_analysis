e402274ea71776bf4772d91b0416709359f471b4
Mijeong Ban <mban1@stevens.edu>
2020-12-03 15:27:10 -0500

    added glove embedding function

--- src/cnn.py
+++ src/cnn.py
@@ -3,8 +3,7 @@ import sys
 import os
 
 # suppress info output from tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 from keras.layers import (
     Input,
     Embedding,
@@ -15,38 +14,37 @@ from keras.layers import (
     Dense,
     Dropout,
 )
-
 from keras.models import Model
 from keras import optimizers
 
 from preprocess import *
 
+
 def build_model(X_train, y_train, X_val, y_val, maxlen, vocab_size, learning_rate):
     input = Input(shape=(maxlen,))
-    embedding = Embedding(vocan_size, 100)  # need to change
-
-    # dropout = SpatialDropout1D(rate=0.0)(embedding)
-    # attention
+    embedding = Embedding(vocab_size, 100, weights=[embedding_matrix], trainable=False)(
+        input
+    )  # need to change
 
     # layers
-    conv0 = Conv1D(filters=64, kernel_size=2, padding="same", activation="relu")(
-        dropout
+    conv0 = Conv1D(filters=128, kernel_size=3, padding="same", activation="relu")(
+        embedding
     )
     pool0 = MaxPooling1D(pool_size=5)(conv0)
 
-    conv1 = Conv1D(filters=128, kernel_size=2, padding="same", activation="relu")(pool0)
-    pool1 = MaxPooling1D(pool_size=5)(conv1)
+    conv1 = Conv1D(filters=128, kernel_size=3, padding="same", activation="relu")(pool0)
+    pool1 = MaxPooling1D(pool_size=5, padding="same")(conv1)
 
-    conv2 = Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(pool1)
-    pool2 = MaxPooling1D(pool_size=5)(conv2)
+    # conv2 = Conv1D(filters=128, kernel_size=3, padding="same", activation="relu")(pool1)
+    # pool2 = MaxPooling1D(pool_size=5, padding='same')(conv2)
 
-    conv3 = Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(pool2)
-    pool3 = MaxPooling1D(pool_size=5)(conv3)
+    # conv3 = Conv1D(filters=128, kernel_size=3, padding="same", activation="relu")(pool2)
+    # pool3 = MaxPooling1D(pool_size=5, padding='same')(conv3)
 
-    flatten = Flatter()(pool3)
+    flatten = Flatten()(pool1)
     dense0 = Dense(64, activation="relu")(flatten)
     dropout = Dropout(0.2)(dense0)
-    dense1 = Dense(3, activation="softmax")(dropout)
+    dense1 = Dense(1, activation="sigmoid")(dropout)
 
     model = Model(input, dense1)
     print(model.summary())
@@ -58,7 +56,7 @@ def build_model(X_train, y_train, X_val, y_val, maxlen, vocab_size, learning_rat
         metrics=["accuracy"],
     )
     model.fit(
-        X_train, y_train, epochs=10, validation_data=(X_val, y_val), batch_size=128
+        X_train, y_train, epochs=3, validation_data=(X_val, y_val), batch_size=128
     )
 
     return model
@@ -72,12 +70,14 @@ if __name__ == "__main__":
         y_val,
         X_test,
         y_test,
-        y_test_categorized,
         maxlen,
         vocab_size,
+        word_index,
     ) = preprocessing()
-    model = build_mode(
+    embedding_matrix = glove_embedding("", vocab_size, word_index)
+
+    model = build_model(
         X_train, y_train, X_val, y_val, maxlen, vocab_size, learning_rate=1e-3
     )
-    loss_and_acc = model.evaluate(X_test, y_test_categorized)
+    loss_and_acc = model.evaluate(X_test, y_test)
     print(loss_and_acc[1])
--- src/preprocess.py
+++ src/preprocess.py
@@ -6,10 +6,10 @@ from keras.utils import to_categorical
 
 
 def preprocessing():
-    df = pd.read_csv("../data/train.csv")
+    df = pd.read_csv("./train.csv")
 
     comments = df["comment_text"].values
-    targets = df["target"].values
+    targets = np.where(df["target"] >= 0.5, True, False) * 1
 
     comments_train, comments_test, y_train, y_test = train_test_split(
         comments, targets, test_size=0.25, shuffle=True
@@ -32,18 +32,12 @@ def preprocessing():
     X_val = pad_sequences(X_val, padding="post", maxlen=maxlen)
     X_test = pad_sequences(X_test, padding="post", maxlen=maxlen)
 
-    # one-hot encode for category labels --> can be removed later nate
-    y_train = to_categorical(y_train)
-    y_val = to_categorical(y_val)
-    y_test_categorized = to_categorical(y_test)
-
     print("Shape of X_train", str(X_train.shape))
     print("Shape of y_train", str(y_train.shape))
     print("Shape of X_val", str(X_val.shape))
     print("Shape of y_val", str(y_val.shape))
     print("Shape of X_test", str(X_test.shape))
     print("Shape of y_test", str(y_test.shape))
-    print("Shape of y_test_categorized", str(y_test_categorized.shape))
 
     return (
         X_train,
@@ -52,7 +46,28 @@ def preprocessing():
         y_val,
         X_test,
         y_test,
-        y_test_categorized,
         maxlen,
         vocab_size,
+        tokenizer.word_index,
     )
+
+
+def glove_embedding(file_path, vocab_size, word_index):
+    embedding_index = {}
+    f = open(file_path)
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype="float32")
+        embedding_index[word] = coefs
+    f.close()
+
+    embedding_matrix = np.zeros((vocab_size, 100))
+    for word, i in word_index.items():
+        embedding_vector = embedding_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    print("The shape of embedding matrix: ", str(embedding_matrix.shape))
+
+    return embedding_matrix
