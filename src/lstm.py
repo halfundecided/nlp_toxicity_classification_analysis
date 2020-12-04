c43a1e3319e6ca5330b0855771b28763c6c40796
Mijeong Ban <mban1@stevens.edu>
2020-12-03 19:06:37 -0500

    fixed lstm

--- src/lstm.py
+++ src/lstm.py
@@ -21,7 +21,6 @@ def build_lstm_model(
     )
     model.add(SpatialDropout1D(0.2))
     model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-    model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
     model.add(Dense(1, activation="sigmoid"))
     model.summary()
     print(model.summary())
